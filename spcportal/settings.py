a579ae39b155f0bc872d3cbb86d1c258c94326c1
Ayush Kumar <anonymouskmr@gmail.com>
2020-12-05 03:02:34 +0530

    aa

--- spcportal/settings.py
+++ spcportal/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY','default_key')
 
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
