050c32d5dff45f23f6c803914813bc551c05edc3
Ayush Kumar <anonymouskmr@gmail.com>
2020-12-05 03:01:00 +0530

    aa

--- spcportal/settings.py
+++ spcportal/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET')
-SECRET_KEY = 'lr^^&mqx8*8pluolw94jl^fu)cd7#e%kycezk!0t6h=h3sinag'
+SECRET_KEY = os.environ.get('SECRET_KEY','default_key')
+
 print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
